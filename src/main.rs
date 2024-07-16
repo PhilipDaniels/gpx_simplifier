5d4db6dd9e34a4d8396baa3a251ae296aa6bb510
Philip Daniels <Philip.Daniels1971@gmail.com>
2024-07-16 10:06:41 +0100

    Don't reprocess files unnecessarily.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::{fs::{read_dir, File}, io::BufWriter, path::{Path, PathBuf}};
-use quick_xml::{events::{attributes::Attribute, BytesStart, Event}, name::QName, Writer};
 use quick_xml::reader::Reader;
+use quick_xml::{
+    events::{attributes::Attribute, BytesStart, Event},
+    name::QName,
+    Writer,
+};
+use std::{
+    fs::{read_dir, File},
+    io::BufWriter,
+    path::{Path, PathBuf},
+};
 
 fn main() {
     let exe_dir = get_exe_dir();
     let input_files = get_list_of_input_files(&exe_dir);
+    if input_files.is_empty() {
+        println!("No .gpx files found");
+        return;
+    }
+
     for f in input_files {
         simplify(&f);
     }
@@ -13,7 +26,15 @@ fn main() {
 fn simplify(input_file: &Path) {
     let mut output_file = input_file.to_owned();
     output_file.set_extension("simplified.gpx");
-    println!("Writing file {:?}", &output_file);
+    if output_file.exists() {
+        println!(
+            "Simplified file {:?} already exists, skipping...",
+            &output_file
+        );
+        return;
+    } else {
+        println!("Writing file {:?}", &output_file);
+    }
 
     // Reading.
     let mut reader = Reader::from_file(input_file).expect("Could not create XML reader");
@@ -26,23 +47,22 @@ fn simplify(input_file: &Path) {
     loop {
         match reader.read_event_into(&mut buf) {
             Ok(Event::Decl(decl)) => {
-                println!("Got Decl: {}", String::from_utf8_lossy(&decl));
                 writer.write_event(Event::Decl(decl)).unwrap();
-            },
+            }
             Ok(Event::Start(e)) => {
                 match e.name().as_ref() {
                     b"gpx" => {
                         println!("Found the gpx tag");
                         writer.write_event(Event::Start(e)).unwrap();
-                    },
+                    }
                     b"trk" => {
                         println!("Found a trk tag");
                         writer.write_event(Event::Start(e)).unwrap();
-                    },
+                    }
                     b"trkseg" => {
                         println!("Found a trkseg tag");
                         writer.write_event(Event::Start(e)).unwrap();
-                    },
+                    }
                     b"trkpt" => {
                         num_trkpts += 1;
 
@@ -62,51 +82,53 @@ fn simplify(input_file: &Path) {
                             idx += 1;
                         }
                         let trimmed_lon = &lon[0..idx + 7];
-                        
+
                         // Make a new trkpt node with the shortened attributes.
-                        let lat2 = Attribute { key: QName(b"lat"), value: trimmed_lat.into() };
-                        let lon2 = Attribute { key: QName(b"lon"), value: trimmed_lon.into() };
+                        let lat2 = Attribute {
+                            key: QName(b"lat"),
+                            value: trimmed_lat.into(),
+                        };
+                        let lon2 = Attribute {
+                            key: QName(b"lon"),
+                            value: trimmed_lon.into(),
+                        };
 
                         let mut e2 = BytesStart::new("trkpt");
                         e2.push_attribute(lat2);
                         e2.push_attribute(lon2);
 
                         writer.write_event(Event::Start(e2)).unwrap();
-                    },
+                    }
                     b"time" => {
                         // Read again to get the text inside the <time>...</time> tags.
                         match reader.read_event_into(&mut buf) {
                             Ok(Event::Text(t)) => {
-                                writer.create_element("time")
-                                .write_text_content(t)
-                                .unwrap();
+                                writer.create_element("time").write_text_content(t).unwrap();
                             }
-                            _ => panic!("Got unexpected XML node, document is probably corrupt")
+                            _ => panic!("Got unexpected XML node, document is probably corrupt"),
                         }
-                    },
-                    _ => ()
-                }
-            },
-            Ok(Event::End(e)) => {
-                match e.name().as_ref() {
-                    b"gpx" => {
-                        writer.write_event(Event::End(e)).unwrap();
-                    },
-                    b"trk" => {
-                        writer.write_event(Event::End(e)).unwrap();
-                    },
-                    b"trkseg" => {
-                        writer.write_event(Event::End(e)).unwrap();
-                    },
-                    b"trkpt" => {
-                        writer.write_event(Event::End(e)).unwrap();
-                    },
+                    }
                     _ => (),
                 }
+            }
+            Ok(Event::End(e)) => match e.name().as_ref() {
+                b"gpx" => {
+                    writer.write_event(Event::End(e)).unwrap();
+                }
+                b"trk" => {
+                    writer.write_event(Event::End(e)).unwrap();
+                }
+                b"trkseg" => {
+                    writer.write_event(Event::End(e)).unwrap();
+                }
+                b"trkpt" => {
+                    writer.write_event(Event::End(e)).unwrap();
+                }
+                _ => (),
             },
             Ok(Event::Eof) => break,
             Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
-            _ => ()
+            _ => (),
         }
 
         buf.clear();
@@ -115,6 +137,10 @@ fn simplify(input_file: &Path) {
     println!("Found {} trkpt nodes", num_trkpts);
 }
 
+// Get a list of all files in the exe_dir that have the ".gpx" extension.
+// Be careful to exclude files that actually end in ".simplified.gpx" -
+// they are output files we already created! If we don't exclude them here,
+// we end up generating ".simplified.simplified.gpx", etc.
 fn get_list_of_input_files(exe_dir: &PathBuf) -> Vec<PathBuf> {
     let mut files = Vec::new();
     let Ok(entries) = read_dir(exe_dir) else {
@@ -127,17 +153,20 @@ fn get_list_of_input_files(exe_dir: &PathBuf) -> Vec<PathBuf> {
         if meta.is_file() {
             let s = &entry.file_name();
             let p = Path::new(s);
-            if let Some(ext) = p.extension()  {
-                if ext == "gpx" {
-                    println!("Found GPX input file {:?}", entry.path());
-                    files.push(entry.path());
+            if let Some(ext) = p.extension() {
+                if ext.to_ascii_lowercase() == "gpx" {
+                    let s = s.to_string_lossy().to_ascii_lowercase();
+                    if !s.ends_with(".simplified.gpx") {
+                        println!("Found GPX input file {:?}", entry.path());
+                        files.push(entry.path());
+                    }
                 }
             }
         }
     }
 
     files
-}   
+}
 
 fn get_exe_dir() -> PathBuf {
     let mut exe_path = std::env::current_exe().unwrap();
